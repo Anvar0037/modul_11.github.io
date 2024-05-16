3882efa5fe8f5d07c89bb399d12f12ac0dd7421a
Anvar0037 <Anvar>
2024-05-16 10:09:28 +0500

    Update project

--- Telegram_bot/handlers.py
+++ Telegram_bot/handlers.py
@@ -8,29 +8,22 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(msg: types.Message):
-    await msg.answer(text="Salom", reply_markup=apple_kb)
+    await msg.answer("Salom", reply_markup=apple_kb)
+
 
 @dp.message(F.func(lambda msg: msg.web_app_data.data))
 async def get_btn(msg: types.Message):
     text = msg.web_app_data.data
-    print(text)
-    title = text.split('/')[0]
-    price = float(text.split('/')[1])
-    quantity = int(text.split('/')[2])
-    print(msg)
-    await msg.answer(text=f"Nomi: {title} \n"
-                          f"Soni: {quantity}\n"
-                          f"Umumiy puli: {quantity * price}")
+    products = text.split("|")
+    summa = 0
+    for i in range(len(products)):
+        title = products[i].split('/')[0]
+        price = int(products[i].split('/')[1])
+        quantity = int(products[i].split('/')[2])
+        await msg.answer(text=f"Mahsulot Nomi: {title}\n"
+                              f"Mahsulot Narxi: {price}\n"
+                              f"Mahsulot Soni: {quantity}\n"
+                              f"Umumiy narxi: {quantity * price}$")
 
 
-@dp.message(F.func(lambda msg: msg.web_app_data.data))
-async def get_btn(msg: types.Message):
-    text1 = msg.web_app_data.data
-    print(text1)
-    title = text1.split('/')[0]
-    price = float(text1.split('/')[1])
-    quantity = int(text1.split('/')[2])
-    print(msg)
-    await msg.answer(text=f"Nomi: {title} \n"
-                          f"Soni: {quantity}\n"
-                          f"Umumiy puli: {quantity * price}")
+
--- app.js
+++ app.js
@@ -228,5 +228,6 @@ abtn4.addEventListener("click", function () {
 
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData(item1 + '|' + item2 + '|' +item3 + '|' + item4);
+    data = item1 + "|" + item2 +  "|" + item3 + "|" + item4
+    tg.sendData(data);
 });
